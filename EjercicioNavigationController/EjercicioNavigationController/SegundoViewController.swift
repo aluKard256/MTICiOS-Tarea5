ea18ca6a8746a97c6c4ae1c74a508e0fd0b47b13
Raúl Escobedo <rauli256@gmail.com>
2015-06-24 18:53:29 -0500

    Primera Actualización
    Esta es la primera actualización.

---
+++ EjercicioNavigationController/EjercicioNavigationController/SegundoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SegundoViewController.swift
+//  EjercicioNavigationController
+//
+//  Created by Raul Ernesto Escobedo Herrera on 6/24/15.
+//  Copyright (c) 2015 Raul Ernesto Escobedo Herrera. All rights reserved.
+//
+
+import UIKit
+
+class SegundoViewController: UIViewController {
+    
+    @IBOutlet weak var switchState: UILabel!
+    
+    var data:String?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        if let label = data{
+            switchState.text = data
+         }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
