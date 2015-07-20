0827645b2db4b304fd143ad4045d8babfa6a7ca2
Raúl Escobedo <rauli256@gmail.com>
2015-07-20 00:10:51 -0500

    Otra actualización
    Esta es otra actualización.

--- NavigationController/EjercicioNavigationController/SegundoViewController.swift
+++ NavigationController/EjercicioNavigationController/SegundoViewController.swift
@@ -13,6 +13,7 @@ class SegundoViewController: UIViewController {
     @IBOutlet weak var switchState: UILabel!
     
     var data:String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
